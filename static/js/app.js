00dd41764c039639e6157ed4df0122f81d6c515a
Christina Gangi <christina@buzzella.com>
2021-05-25 17:36:43 -0400

    Event handler created

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,11 @@
-function demo(subject) {
+// Global variable(s)
+const dropdown = d3.select('#selDataset');
+
+function demo(subjectID) {
+  console.log(`- demo function called. Subject: ${subjectID}`);
   d3.json("static/data/samples.json").then((data) => {
     var metadata = data.metadata;
-    var results = metadata.filter(sbj_demo => sbj_demo.id == subject);
+    var results = metadata.filter(sbj_demo => sbj_demo.id == subjectID);
     var default_result = results[0];
     var panel = d3.select('#sample-metadata');
     panel.html('');
@@ -9,81 +13,47 @@ function demo(subject) {
       panel.append('h6').text(`${key.toUpperCase()}: ${value}`);
     });
   });
+  console.log('- demo function completed');
 }
 
-function build_charts(subject) {
+function build_charts(subjectID) {
+  console.log(`- build_charts function called. Subject: ${subjectID}`);
   d3.json("static/data/samples.json").then((data) => {
     var samples = data.samples;
-    var results = samples.filter(sbj_demo => sbj_demo.id == subject);
+    var results = samples.filter(sbj_demo => sbj_demo.id == subjectID);
     var default_result = results[0];
     var otu_ids = default_result.otu_ids;
     var otu_labels = default_result.otu_labels;
     var sample_values = default_result.sample_values;
-    
-
-
+  });
+  console.log('- build_charts function completed');
 }
 
 function init() {
-  var dropdown = d3.select('#selDataset');
+  console.log('Init function called');
   d3.json("static/data/samples.json").then((data) => {
     var names = data.names;
     names.forEach((subject) => {
       dropdown.append('option').text(subject).property('value', subject);
     });
+    // Default to 1st sample ID
     var def_subject = names[0];
-    // call functions to build charts and panel
+    // Populate Demographic Info
     demo(def_subject);
+    // Populate Bar Chart, Bubble Chart, & Gauge Chart
+    build_charts(def_subject);
+    console.log('Init function completed');
+
   });
 }
 
+function optionChanged(subjectID) {
+  console.log(`optionChanged function called. Subject: ${subjectID}`)
+  // Update Demographic Info
+  demo(subjectID);
+  // Update Bar Chart, Bubble Chart, & Gauge Chart
+  build_charts(subjectID);
+  console.log('optionChanged function completed');
+}
+
 init();
-
-  // Pull array of sample ids
-  // const sample_ids = console.log(data.samples.map(sample => sample.id));
-  
-  // d3.select('#selDataset').selectAll('option').data(sample_ids);
-
-  // options.enter()
-  //   .append('option')
-  //   .attr('value', function(d) {
-  //     return d.value;
-  //   })
-  //   .text(function(d) {
-  //     return d.text;
-  //   });
-
-  // console.log(sample_ids.indexOf('940'))
-
-
-
-
-
-
-
-
-
-// 
-// const file_path = 'static/data/samples.json'
-
-
-
-    // data.array.forEach(element => {
-    //   // element.samples
-      
-    // console.log(element.samples);
-    // });
-
- 
-
-
-
-
-
-// Populate dropdown list: selDataset
-// d3.select("#selDataset")
-
-// sample-metadata
-// bar
-// gauge
-// bubble
